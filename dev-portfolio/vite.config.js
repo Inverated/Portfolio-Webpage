b7f9a38fca710aeb9490321d58f0aa2207d2568c
Inverated <120025059+Inverated@users.noreply.github.com>
2026-09-05 19:13:47 +0800

    Rename base

--- dev-portfolio/vite.config.js
+++ dev-portfolio/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   build: {
     outDir: 'build',
   },
-  base: '/my-portfolio/',
+  base: '/Portfolio-Webpage/',
   test: {
     globals: true,
     environment: 'jsdom',
