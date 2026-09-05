5c7237315f033cb58dd613b777c43e0802cb9aef
Inverated <120025059+Inverated@users.noreply.github.com>
2026-09-05 19:27:30 +0800

    Update endpoints.js

--- dev-portfolio/src/constants/endpoints.js
+++ dev-portfolio/src/constants/endpoints.js
@@ -1,15 +1,17 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
 const endpoints = {
-  navbar: `${import.meta.env.BASE_URL}profile/navbar.json`,
-  routes: `${import.meta.env.BASE_URL}profile/routes.json`,
-  home: `${import.meta.env.BASE_URL}profile/home.json`,
-  social: `${import.meta.env.BASE_URL}profile/social.json`,
-  about: `${import.meta.env.BASE_URL}profile/about.json`,
-  skills: `${import.meta.env.BASE_URL}profile/skills.json`,
-  education: `${import.meta.env.BASE_URL}profile/education.json`,
-  experiences: `${import.meta.env.BASE_URL}profile/experiences.json`,
-  projects: `${import.meta.env.BASE_URL}profile/projects.json`,
-  hobbies: `${import.meta.env.BASE_URL}profile/hobbies.json`,
-  community: `${import.meta.env.BASE_URL}profile/community.json`,
+  navbar: `${BASE_URL}profile/navbar.json`,
+  routes: `${BASE_URL}profile/routes.json`,
+  home: `${BASE_URL}profile/home.json`,
+  social: `${BASE_URL}profile/social.json`,
+  about: `${BASE_URL}profile/about.json`,
+  skills: `${BASE_URL}profile/skills.json`,
+  education: `${BASE_URL}profile/education.json`,
+  experiences: `${BASE_URL}profile/experiences.json`,
+  projects: `${BASE_URL}profile/projects.json`,
+  hobbies: `${BASE_URL}profile/hobbies.json`,
+  community: `${BASE_URL}profile/community.json`,
 };
 
 export default endpoints;
